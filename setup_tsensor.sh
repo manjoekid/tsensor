d2099af72bf3f49c372993eba4f2ea2bf5199de2
Rafael <rafaelkcosta@gmail.com>
2024-04-05 16:32:48 -0300

    created aditional folder tsensor for git download

--- setup_tsensor.sh
+++ setup_tsensor.sh
@@ -30,8 +30,8 @@ echo "Virtual environment deactivated successfully."
 echo "Changing permissions for the tsensor service..."
 chmod +x tsensor_py_service.sh || echo "Failed to change permissions."
 
-sudo chown -R root:www-data /home/tsensor/tsensor_py/ || echo "Failed to change permissions."
-sudo chmod -R 775 /home/tsensor/tsensor_py/ || echo "Failed to change permissions."
+sudo chown -R root:www-data /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
+sudo chmod -R 775 /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
 
 echo "Starting tsensor service..."
 sudo cp tsensor.service /etc/systemd/system/ || echo "Failed to start tsensor service."
@@ -39,7 +39,7 @@ sudo systemctl enable tsensor.service || echo "Failed to start tsensor service."
 sudo systemctl start tsensor.service || echo "Failed to start tsensor service."
 
 echo "Tsensor finished. Starting web server installation..."
-cd /home/tsensor/tsensor_web || { echo "Failed to change directory. Exiting."; exit 1; }
+cd /home/tsensor/tsensor/tsensor_web || { echo "Failed to change directory. Exiting."; exit 1; }
 
 
 echo "Installing necessary packages..."
@@ -70,8 +70,8 @@ echo "Virtual environment deactivated successfully."
 echo "Starting tsensor web service..."
 sudo cp flask.service /etc/systemd/system/  || { echo "Failed to start tsensor web service. Exiting."; exit 1; } 
 
-sudo chown -R root:www-data /home/tsensor/tsensor_web/   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
-sudo chmod -R 775 /home/tsensor/tsensor_web/   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
+sudo chown -R root:www-data /home/tsensor/tsensor/tsensor_web/   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
+sudo chmod -R 775 /home/tsensor/tsensor/tsensor_web/   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
 
 sudo systemctl daemon-reload   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
 
