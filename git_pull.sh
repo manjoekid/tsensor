a59c2e8a92dd7c6a977783a15976d9af3b3f6ce1
Rafael <rafaelkcosta@gmail.com>
2024-04-17 07:36:53 -0300

    git script

--- git_pull.sh
+++ git_pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo git stash
-sudo git pull
-sudo chown -R root:www-data /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
-sudo chmod -R 775 /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
-sudo ./virtualenv_py/bin/python3 ./test_serial.py
+git stash
+git pull
+chown -R root:www-data /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
+chmod -R 775 /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
+/home/tsensor/tsensor/tsensor_py/virtualenv_py/bin/python3 /home/tsensor/tsensor/tsensor_py/test_serial.py
