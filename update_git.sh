2b7b3b7c042624ee0adb3c709de3ccce944fecec
Rafael <rafaelkcosta@gmail.com>
2024-05-07 07:57:41 -0300

    update git script

---
+++ update_git.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Function to download files from GitHub repository
+update_git() {
+    # Define repository URL
+    repo_url="https://github.com/manjoekid/tsensor"
+
+    # Define destination folder
+    destination_folder="/home/tsensor/"
+
+    echo "Updating Git repository..."
+    cd  /home/tsensor/tsensor || { echo "Error: Folder not found."; exit 1; }
+
+
+    echo "Reseting local repository..."
+    sudo git reset || { echo "Erro: Não conseguiu resetar o repositório local."; exit 1; }
+
+    # Download repository files
+    echo "Pulling repository from GitHub..."
+    sudo git pull || { echo "Erro: Não conseguiu dar o pull no repositório do Github."; exit 1; }
+    
+    echo "Repository pulled successfully."
+
+    echo "Changing permissions for the tsensor service..."
+    chmod +x tsensor_py_service.sh || echo "Failed to change permissions."
+
+    sudo chown -R root:www-data /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
+    sudo chmod -R 775 /home/tsensor/tsensor/tsensor_py/ || echo "Failed to change permissions."
+
+    sudo chown -R root:www-data /home/tsensor/tsensor/tsensor_web/   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
+    sudo chmod -R 775 /home/tsensor/tsensor/tsensor_web/   || { echo "Failed to start tsensor web service. Exiting."; exit 1; }
+
+}
+
+stop_service() {
+    sudo systemctl stop tsensor || { echo "Erro: Não encontrou serviço tsensor."; exit 1; }
+    sudo systemctl stop flask || { echo "Erro: Não encontrou serviço flask."; exit 1; }
+    echo "Serviços parados."
+}
+
+start_service() {
+    sudo systemctl start tsensor || { echo "Erro: Não subiu serviço tsensor."; exit 1; }
+    sudo systemctl start flask || { echo "Erro: Não subiu serviço flask."; exit 1; }
+    echo "Serviços iniciados."
+}
+
+echo "Iniciando atualização do repositório Git..."
+
+# Call the functions
+stop_service
+update_git
+start_service
+
+echo "Atualização realizada com sucesso."
